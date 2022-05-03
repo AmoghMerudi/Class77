103ad54874dffa04ad7efeb478f71b6d8db04618
AmoghMerudi <73833979+AmoghMerudi@users.noreply.github.com>
2022-05-03 15:42:52 +0530

    Add files via upload

---
+++ Class77/screens/ISSLocationScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default class ISSLocationScreen extends React.Component{
+    render(){
+        return(
+            <View>
+                <Text>Welcome to ISSLocationScreen</Text>
+            </View>
+        );
+    }
+}
